1a5a85b5b5f1d01fa38723ced26774c3e904e1a9
Tim Kral <timkral@gmail.com>
2017-02-03 10:00:04 -0800

    Add producer/consumer config interface

---
+++ config/src/main/java/net/spals/appbuilder/config/ConsumerConfig.java
@@ -0,0 +1,44 @@
+package net.spals.appbuilder.config;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import org.inferred.freebuilder.FreeBuilder;
+
+import java.util.Optional;
+
+/**
+ * Common configuration for an event consumer.
+ *
+ * @author tkral
+ */
+@FreeBuilder
+@JsonDeserialize(builder = ConsumerConfig.Builder.class)
+public interface ConsumerConfig {
+
+    String ACTIVE_KEY = "active";
+    String CHANNEL_KEY = "channel";
+    String GLOBAL_ID_KEY = "globalId";
+    String SOURCE_KEY = "source";
+
+    @JsonProperty(ConsumerConfig.ACTIVE_KEY)
+    Optional<Boolean> getActive();
+
+    @JsonIgnore
+    default boolean isActive() {
+        return getActive().orElse(true);
+    }
+
+    @JsonProperty(ConsumerConfig.CHANNEL_KEY)
+    String getChannel();
+
+    @JsonProperty(ConsumerConfig.GLOBAL_ID_KEY)
+    String getGlobalId();
+
+    @JsonProperty(ConsumerConfig.SOURCE_KEY)
+    String getSource();
+
+    String getTag();
+
+    class Builder extends ConsumerConfig_Builder {  }
+}
