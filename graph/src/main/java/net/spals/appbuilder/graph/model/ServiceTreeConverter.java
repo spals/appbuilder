e0b01916a2c64dc9fd3968cf81cbc3e9016c49a5
Tim Kral <timkral@gmail.com>
2017-10-19 23:16:10 -0700

    Better data structure for faster ServiceDAG to ServiceTree conversion

--- graph/src/main/java/net/spals/appbuilder/graph/model/ServiceTreeConverter.java
+++ graph/src/main/java/net/spals/appbuilder/graph/model/ServiceTreeConverter.java
@@ -1,6 +1,8 @@
 package net.spals.appbuilder.graph.model;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 import org.jgrapht.DirectedGraph;
 import org.jgrapht.event.EdgeTraversalEvent;
 import org.jgrapht.event.TraversalListenerAdapter;
@@ -8,10 +10,7 @@ import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.EdgeReversedGraph;
 import org.jgrapht.traverse.TopologicalOrderIterator;
 
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.Collection;
 
 import static net.spals.appbuilder.graph.model.ServiceDAGConverter.ApplicationVertex.APPLICATION_VERTEX_KEY;
 import static net.spals.appbuilder.graph.model.ServiceTreeVertex.createTreeChild;
@@ -35,7 +34,8 @@ class ServiceTreeConverter {
 
         // Walk the graph in topographical order and use the tree conversion listener
         // to convert the DAG into a tree
-        final ServiceTreeConversionListener listener = new ServiceTreeConversionListener(serviceTree, reversedGraph);
+        final ServiceTreeConversionListener listener = new ServiceTreeConversionListener(applicationVertex,
+            serviceTree, reversedGraph);
         topoOrder.addTraversalListener(listener);
         while (topoOrder.hasNext()) {
             topoOrder.next();
@@ -58,14 +58,15 @@ class ServiceTreeConverter {
         private final ServiceTree serviceTree;
         private final DirectedGraph<IServiceDAGVertex<?>, DefaultEdge> traversedGraph;
 
-        private final Deque<IServiceTreeVertex<?>> visitedVertices = new LinkedList<>();
+        private final Multimap<IServiceDAGVertex<?>, IServiceTreeVertex<?>> visitedVertices = HashMultimap.create();
 
-        ServiceTreeConversionListener(final ServiceTree serviceTree,
+        ServiceTreeConversionListener(final IServiceDAGVertex<?> dagRoot,
+                                      final ServiceTree serviceTree,
                                       final DirectedGraph<IServiceDAGVertex<?>, DefaultEdge> traversedGraph) {
             this.serviceTree = serviceTree;
             this.traversedGraph = traversedGraph;
 
-            visitedVertices.addLast(serviceTree.getRoot());
+            visitedVertices.put(dagRoot, serviceTree.getRoot());
         }
 
         @Override
@@ -73,22 +74,20 @@ class ServiceTreeConverter {
             final IServiceDAGVertex<?> dagSource = traversedGraph.getEdgeSource(e.getEdge());
             final IServiceDAGVertex<?> dagTarget = traversedGraph.getEdgeTarget(e.getEdge());
 
-            final Set<IServiceTreeVertex<?>> treeParents = findVisitedDAGVertices(dagSource);
+            final Collection<IServiceTreeVertex<?>> treeParents = findVisitedDAGVertices(dagSource);
             treeParents.forEach(treeParent -> addTreeVertex(treeParent, dagTarget));
         }
 
         private void addTreeVertex(final IServiceTreeVertex<?> treeParent, final IServiceDAGVertex<?> dagChild) {
             final IServiceTreeVertex<?> treeChild = createTreeChild(dagChild, treeParent);
-            visitedVertices.addLast(treeChild);
+            visitedVertices.put(dagChild, treeChild);
 
             serviceTree.addVertex(treeChild);
             serviceTree.addEdge(treeParent, treeChild);
         }
 
-        private Set<IServiceTreeVertex<?>> findVisitedDAGVertices(final IServiceDAGVertex<?> dagVertex) {
-            return visitedVertices.stream()
-                .filter(visitedVertex -> ((ServiceTreeVertex)visitedVertex).getDelegate().equals(dagVertex))
-                .collect(Collectors.toSet());
+        private Collection<IServiceTreeVertex<?>> findVisitedDAGVertices(final IServiceDAGVertex<?> dagVertex) {
+            return visitedVertices.get(dagVertex);
         }
     }
 }
