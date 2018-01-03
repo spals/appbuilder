c6d89f73370e920f40b6a1fd7b3e352fa9245cda
Tim Kral <timkral@gmail.com>
2018-01-03 09:38:41 -0800

    Allow MongoDB MapStore to be configured with single URL

--- mapstore-mongodb/src/main/scala/net/spals/appbuilder/mapstore/mongodb/MongoDatabaseProvider.scala
+++ mapstore-mongodb/src/main/scala/net/spals/appbuilder/mapstore/mongodb/MongoDatabaseProvider.scala
@@ -1,13 +1,13 @@
 package net.spals.appbuilder.mapstore.mongodb
 
-import javax.validation.constraints.NotNull
-
+import com.google.common.annotations.VisibleForTesting
 import com.google.inject.{Inject, Provider}
-import com.mongodb.MongoClient
 import com.mongodb.client.MongoDatabase
+import com.mongodb.{MongoClient, MongoClientURI}
 import com.netflix.governator.annotations.Configuration
 import net.spals.appbuilder.annotations.config.ApplicationName
 import net.spals.appbuilder.annotations.service.AutoBindProvider
+import org.slf4j.LoggerFactory
 
 /**
   * @author tkral
@@ -17,13 +17,27 @@ private[mongodb] class MongoDatabaseProvider @Inject() (
   @ApplicationName applicationName: String,
   mongoClient: MongoClient
 ) extends Provider[MongoDatabase] {
+  private val LOGGER = LoggerFactory.getLogger(classOf[MongoDatabaseProvider])
 
-  @NotNull
   @Configuration("mapStore.mongoDB.database")
   @volatile
+  @VisibleForTesting
   private[mongodb] var configuredDatabaseName: String = null
 
-  private lazy val databaseName = Option(configuredDatabaseName).getOrElse(applicationName)
+  @Configuration("mapStore.mongoDB.url")
+  @volatile
+  @VisibleForTesting
+  private[mongodb] var url: String = null
 
-  override def get(): MongoDatabase = mongoClient.getDatabase(databaseName)
+  @VisibleForTesting
+  private[mongodb] lazy val databaseName = {
+    val mongoClientURI = Option(url).map(new MongoClientURI(_))
+    mongoClientURI.map(_.getDatabase)
+      .getOrElse(Option(configuredDatabaseName).getOrElse(applicationName))
+  }
+
+  override def get(): MongoDatabase = {
+    LOGGER.info(s"MongoDB database is: ${databaseName}")
+    mongoClient.getDatabase(databaseName)
+  }
 }
